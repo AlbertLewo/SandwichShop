15e753e3ed6add98c2e490da3fb579a112cecd7b
AlbertLewo <albert.tannous@cbhslewisham.nsw.edu.au>
2022-06-06 21:01:38 +1000

    Added/Updated repository: 6/6/22

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import './App.css';
+import { Typography, Card, CardContent, Grid, TextField } from '@material-ui/core'
   
 const Contact = () => {
+
   return (
-    <div>
-      <h1>Contact Us Page</h1>
+    <div className="App">
+
+      <Typography align="center" id="modal-modal-title" variant="h2" component="h2" >
+        Contact Us Page
+      </Typography>
+
+      <Card>
+        <CardContent>
+          <Grid container={1} m={2} p={5} direction={"row"} spacing={5} >
+
+          <Grid xs={16} sm={2} item>
+            <TextField label="First Name" placeholder="Enter your first name" variant="outlined" fullWidth/>
+          </Grid>
+
+          <Grid xs={16} sm={2} item>
+            <TextField label="Last Name" placeholder="Enter your first name" variant="outlined" fullWidth/>
+          </Grid>
+
+          </Grid>
+        </CardContent>
+      </Card>
+
     </div>
+    
   );
 };
 
